47801fd3059515b83a802b9b75115b63793af388
scott-wyatt <scottwyatt86@gmail.com>
2016-07-16 12:53:31 -0400

    Import generator-trails questions

--- src/app/questions/index.js
+++ src/app/questions/index.js
@@ -1,84 +1,10 @@
 'use strict';
 
-//Questions
-export default [
-
-// web-engine
-{
-  type: 'list',
-  name: 'web-engine',
-  message: 'Choose a Web Server',
-  choices: [
-    'hapi',
-    'express',
-    'koa (not supported yet)',
-    'restify (not supported yet)',
-    'other'
-  ],
-  defaults: 'hapi'
-},
-{
-  when: responses => {
-    return responses['web-engine'] == 'other'
-  },
-  name: 'web-engine-other',
-  message: 'What is the name of this web engine?'
-},
-{
-  type: 'list',
-  when: responses => {
-    return responses['web-engine'] == 'express'
-  },
-  name: 'express-version',
-  message: 'What express version do you want to use ?',
-  choices: [
-    '4',
-    '5',
-    'other'
-  ],
-},
-{
-  when: responses => {
-    return responses['express-version'] == 'other'
-  },
-  name: 'express-version-other',
-  message: 'What express version do you want to use (example 4.13.3) ?'
-},
-
-// orm-engine
-{
-  type: 'list',
-  name: 'orm-engine',
-  message: 'Choose an ORM',
-  choices: [
-    'waterline',
-    'mongoose',
-    'knex',
-    'sequelize',
-    'bookshelf',
-    'js-data',
-    'none',
-    'other'
-  ],
-  defaults: 'waterline'
-},
-{
-  when: responses => {
-    return responses['orm-engine'] == 'other'
-  },
-  name: 'orm-engine-other',
-  message: 'What is the name of this ORM ?'
-},
-
-// footprints
-{
-  type: 'confirm',
-  when: responses => {
-    return responses['orm-engine'] != 'none'
-  },
-  name: 'footprints',
-  message: 'Do you want to use Footprints (automatic REST API from models) ?'
-},
+import trailsQuestions from 'generator-trails/generators/app/questions'
+ 
+const questions = trailsQuestions.concat(
+//Treefrog Questions
+[
 // frontend
 {
   type: 'list',
@@ -170,5 +96,6 @@ export default [
   },
   name: 'outDir',
   message: 'What is the name of the Output Directory?'
-}];
-module.exports = exports['default']
+}])
+
+export default questions
